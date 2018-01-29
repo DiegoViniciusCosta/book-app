a22758fb734551a6c2efdfbc35a776daa1930cca
Gabriel Lamas <gabrielldarts@gmail.com>
2018-01-29 16:22:39 -0200

    Atualizações Book-app

---
+++ js/custom.js
@@ -0,0 +1,49 @@
+$('.filtroIcone').on('click', function(){
+	$('.filter').toggle();
+});
+
+$('nav').on('click', function(){
+	$('nav .flex-area').toggle();
+});
+
+
+
+
+$('.bt-login').on('click', function(){
+	$('.se-login').toggle();
+});
+$('.close-login').on('click', function(){
+	$('.se-login').toggle();
+});
+
+
+
+
+$('.cadastrar-com-email').on('click', function(){
+	$('.se-cadastrar').toggle();
+});
+$('.close-cadastrar').on('click', function(){
+	$('.se-cadastrar').toggle();
+});
+
+
+
+
+
+$('.esqueci-minha-senha').on('click', function(){
+	$('.se-esqueci').toggle();
+});
+$('.close-esqueci').on('click', function(){
+	$('.se-esqueci').toggle();
+});
+
+
+
+
+
+$('.cadastrar-novo-livro').on('click', function(){
+	$('.se-cadastrar-livro').toggle();
+});
+$('.close-cadastrar-livro').on('click', function(){
+	$('.se-cadastrar-livro').toggle();
+});
