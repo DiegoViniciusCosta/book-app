6d07eb04f42a6bb17a7aaca765faf44fc2863c7e
Gabriel Lamas <gabrielldarts@gmail.com>
2018-06-15 19:27:50 -0300

    ficha pure

--- ficha-pure-js/js/custom.js
+++ ficha-pure-js/js/custom.js
@@ -12,59 +12,72 @@ let criaCadastro = function(event) {
     inputCnh = $('#cnh')
     inputEstadoCivil = $('#estadoCivil')
 
-    inputOcupacao = $('#ocupacao')
+    inputOcupacoes = $('#ocupacoes')
     inputProfissao = $('#profissao')
-    <input type="text" id="renda" placeholder="Renda">
-    <input type="text" id="nomeEmpresa" placeholder="Nome da Empresa">
+    inputRenda = $('#renda')
+    inputEmpresa = $('#nomeEmpresa')
 
-    <!--INPUT TELEFONE -->
-    <input type="text" id="telefone" placeholder="telefone">
-    <!--/INPUT TELEFONE-->
+    inputTelefone = $('#telefone')
+    inputTelefone2 = $('#telefone2')
+    inputTelefone3 = $('#telefone3')
     
-    <input type="email" id="email" placeholder="Email">
+    inputEmail = $('#email')
 
-    <!--ENDEREÇO-->
-    <input type="text" id="cep" placeholder="CEP">
-    <input type="endereco" id="endereco" placeholder="Endereço">
-    <input type="text" id="numero" placeholder="Número">
-    <input type="text" id="complemento" placeholder="Complemento">
-    <input type="text" id="bairro" placeholder="Bairro">
-    <input type="text" id="cidade" placeholder="Cidade">
-    <input type="text" id="estado" placeholder="Estado">
-    <!--/ENDEREÇO-->
-
-    <!--Dados do carro-->
-    <select name="marcas" id="marcas">
-        <option value="onix">Onix</option>
-    </select>
-    <select name="modelos" id="modelos">
-        <option value="1.4 Altras">1.4 Altras</option>
-    </select>
-    <select name="versoes" id="versoes">
-        <option value="versaoPa">Versão Pá</option>
-    </select>
-    <select name="cores" id="cores">
-        <option value="vermelho">Vermelho</option>
-    </select>
-    <select name="marcas" id="marcas">
-        <option value="marca">Marca</option>
-    </select>
+    inputCep = $('#cep')
+    inputEndereco = $('#endereco')
+    inputNumero = $('#numero')
+    inputComplemento = $('#complemento')
+    inputBairro = $('#bairro')
+    inputCidade = $('#cidade')
+    inputEstado = $('#estado')
     
-    <!--PERÍODO DE CONTRATO-->
-    <input type="text" id="periodoContrato" placeholder="Período de Contrato">
-    <input type="text" id="pacotekm" placeholder="Pacote de KM">
-    <!--/PERÍODO DE CONTRATO-->
-
-    <!--VALOR DA PARCELA-->
-    <h2>Valor da parcela</h2>
-    <!--/VALOR DA PARCELA-->
-
+    inputMarcas = $('#marcas')
+    inputModelos = $('#modelos')
+    inputVersoes = $('#versoes')
+    inputCores = $('#cores')
+    inputPlaca = $('#placas')
+    
+    inputPeriodosContratos = document.getElementsByName('periodoContrato');
+    inputPacotesKm = document.getElementsByName('pacotekm');
 
     let dadosCliente = {
         nome: inputNome.value,
         nomeMae: inputNomeMae.value,
         data: inputData.value,
-        sexo: inputSexo[1].checked ? 'masculino' : 'Feminino' 
+        sexo: inputSexo[0].checked ? this.inputSexo[0].value : this.inputSexo[1].value,
+
+        cpf: inputCpf.value,
+        rg: inputRg.value,
+        cnh: inputCnh.value,
+        estadoCivil: inputEstadoCivil.value,
+
+        ocupacao : inputOcupacoes.value,
+        profissao : inputProfissao.value,
+        renda : inputRenda.value,
+        empresa : inputEmpresa.value,
+
+        telefone : inputTelefone.value,
+        telefone2 : inputTelefone2.value,
+        telefone3 : inputTelefone3.value,
+        
+        email : inputEmail.value,
+
+        cep : inputCep.value,
+        endereco : inputEndereco.value,
+        numero : inputNumero.value,
+        complemento : inputComplemento.value,
+        bairro : inputBairro.value,
+        cidade : inputCidade.value,
+        estado : inputEstado.value,
+        
+        marcas : inputMarcas.value,
+        modelos : inputModelos.value,
+        versoes : inputVersoes.value,
+        cores : inputCores.value,
+        placa : inputPlaca.value,
+        
+        contrato: inputPeriodosContratos[0].checked ? inputPeriodosContratos[0].value : '20',
+        pacotes: inputSexo[0].checked ? this.inputSexo[0].value : this.inputSexo[1].value,
     }
 
     console.log(dadosCliente)
